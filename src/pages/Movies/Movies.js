f9808dc4851c0ec17adc02e581ac2c470c0419d9
Jubz-Cherry <juliapnascimento15@gmail.com>
2025-11-04 16:11:15 -0300

    add page for movies

--- src/pages/Movies/Movies.js
+++ src/pages/Movies/Movies.js
@@ -1,9 +1,28 @@
+import React, { useEffect, useState } from "react";
+import styles from "./Movies.module.css";
 
+export default function Movies() {
+  const [movies, setMovies] = useState([]);
 
-function Movies(){
-    return(
-        <h1>oi</h1>
-    )
+  useEffect(() => {
+    fetch("http://localhost:3000/movies")
+      .then((res) => res.json())
+      .then((data) => setMovies(data))
+      .catch((err) => console.error("Erro ao buscar filmes:", err));
+  }, []);
+
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>🎬 Catálogo de Filmes</h1>
+      <div className={styles.grid}>
+        {movies.map((movie) => (
+          <div key={movie.id} className={styles.card}>
+            <h2>{movie.title}</h2>
+            <p>{movie.genre}</p>
+            <span>⭐ {movie.rating}</span>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
-
-export default Movies;
